749465f0685dfee61ea3fd76a1150cb4fb4d576e
Arve Slenes <arve.slenes@gmail.com>
2019-02-28 00:49:48 +0100

    Endring i oppløysing

--- scripts/webcam_live.sh
+++ scripts/webcam_live.sh
@@ -2,7 +2,8 @@
 
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-/home/pi/projects/scripts/reset_cam.sh
+/home/pi/projects/WebCamRaspberryPi/scripts/reset_cam.sh
 
 #fswebcam -r 1280x720 --delay 5 --no-banner /home/pi/webcam/livecam.jpg
-sudo fswebcam -r 960x720 --delay 3 --title Haukaas /home/pi/webcam/livecam.jpg
+#sudo fswebcam -r 960x720 --delay 3 --title Haukaas /home/pi/webcam/livecam.jpg
+sudo fswebcam -r 640x480 --delay 3 --title Haukaas /home/pi/webcam/livecam.jpg
